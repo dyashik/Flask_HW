e6dff49a76eeb697e01d6c00eb70a74847060ba6
dyashik <yashikdhanaraj@gmail.com>
2022-03-21 14:08:51 -0700

    for hp

---
+++ app/home.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask import escape
+
+myobj = Flask(__name__)
+name = "Lisa"
+city_names = ["Paris", "London", "Rome", "Tahiti"]
+
+
+#view function
+@myobj.route("/")
+@myobj.route("/index.html")
+def home():
+
+    return render_template("home.html", name = name, city_names = city_names)
+
+myobj.run()
